cf94700cae285e555dacc6d61119f08973df15a2
Vincent Langat <104572853+VincentLangat033@users.noreply.github.com>
2023-08-08 05:23:26 +0300

    restore previous maim.py files

--- main.py
+++ main.py
@@ -1,79 +1,79 @@
-# from termcolor import colored
+from termcolor import colored
 
-# pos_cli = ("""
-# ----------------------------------
+pos_cli = ("""
+----------------------------------
 
-# *** Dear Customer, ***
-# ----------------------------------
+*** Dear Customer, ***
+----------------------------------
 
-# *** Welcome to Python POS CLI  ***
+*** Welcome to Python POS CLI  ***
 
-# ----------------------------------
-# *** You will perform the following operations ***
-#     """)
+----------------------------------
+*** You will perform the following operations ***
+    """)
 
 
-# def main_menu():
+def main_menu():
 
-#     print(colored(pos_cli, "yellow"))
-#     print(colored("1. Customer Operations ", "blue"))
-#     print(colored("2. Product Operations ", "blue"))
-#     print(colored("3. Purchase Operations", "blue"))
-#     print(colored("""
-# ________________________________________________
-#     """, "yellow"))
-#     print(colored("What do you wish to do? ", "blue"))
-#     value = int(input(colored("Enter operation you wish to perform : ", "blue")))
-#     if value == 1:
-#         print(colored("""
-# ---------------------------------------------
+    print(colored(pos_cli, "yellow"))
+    print(colored("1. Customer Operations ", "blue"))
+    print(colored("2. Product Operations ", "blue"))
+    print(colored("3. Purchase Operations", "blue"))
+    print(colored("""
+________________________________________________
+    """, "yellow"))
+    print(colored("What do you wish to do? ", "blue"))
+    value = int(input(colored("Enter operation you wish to perform : ", "blue")))
+    if value == 1:
+        print(colored("""
+---------------------------------------------
 
-# ***   You are at  Customer Operations Column:   ***
+***   You are at  Customer Operations Column:   ***
     
-# ---------------------------------------------
-#         """, "yellow"))
-#         from customer import customer_menu
-#         customer_menu.customer_menu()
-#     elif value == 2:
-#         from product import product_menu
-#         product_menu.product_menu()
-#     elif value == 3:
-#         from purchases import purchase_menu
-#         purchase_menu.make_purchases()
+---------------------------------------------
+        """, "yellow"))
+        from customer import customer_menu
+        customer_menu.customer_menu()
+    elif value == 2:
+        from product import product_menu
+        product_menu.product_menu()
+    elif value == 3:
+        from purchases import purchase_menu
+        purchase_menu.make_purchases()
  
-#     else:
-#         print("""
-# `````````````````````````````
-#         """)
-#         print(colored("Try again", "red"))
-#         main_menu()
+    else:
+        print("""
+`````````````````````````````
+        """)
+        print(colored("Try again", "red"))
+        main_menu()
 
 
-# main_menu()
+main_menu()
 
 
-import random
-import openpyxl
+# import random
+# import openpyxl
 
-def generate_random_numbers(num_numbers, start_range, end_range):
-    random_numbers = [random.randint(start_range, end_range) for _ in range(num_numbers)]
-    return random_numbers
+# def generate_random_numbers(num_numbers, start_range, end_range):
+#     random_numbers = [random.randint(start_range, end_range) for _ in range(num_numbers)]
+#     return random_numbers
 
-def export_to_excel(numbers):
-    workbook = openpyxl.Workbook()
-    sheet = workbook.active
+# def export_to_excel(numbers):
+#     workbook = openpyxl.Workbook()
+#     sheet = workbook.active
 
-    for row_num, number in enumerate(numbers, start=1):
-        cell = sheet.cell(row=row_num, column=1, value=number)
+#     for row_num, number in enumerate(numbers, start=1):
+#         cell = sheet.cell(row=row_num, column=1, value=number)
 
-    workbook.save("random_numbers.xlsx")
-    print("Random numbers exported to random_numbers.xlsx")
+#     workbook.save("random_numbers.xlsx")
+#     print("Random numbers exported to random_numbers.xlsx")
 
-if __name__ == "__main__":
-    num_numbers = 800
-    start_range = 15000000
-    end_range = 37000000
+# if __name__ == "__main__":
+#     num_numbers = 800
+#     start_range = 15000000
+#     end_range = 37000000
 
-    random_numbers = generate_random_numbers(num_numbers, start_range, end_range)
-    export_to_excel(random_numbers)
+#     random_numbers = generate_random_numbers(num_numbers, start_range, end_range)
+#     export_to_excel(random_numbers)
 
